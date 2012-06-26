2e413f7ab0778245529c387ab1dcd1e4d9f1d25c
Jeroen Wesbeek <work@osx.eu>
2012-06-26 16:39:32 +0200

    Upgraded to Grails 2.0.4 and moved to Grails Central
    see
    http://grails.github.com/grails-howtos/en/migrateToTheNewCentralReposito
    ry.html

--- AjaxflowGrailsPlugin.groovy
+++ AjaxflowGrailsPlugin.groovy
@@ -19,48 +19,64 @@
  *  $Date$
  */
 class AjaxflowGrailsPlugin {
-	def version		= "0.2.2"
-	def grailsVersion	= "1.3.4 > *"
-	def dependsOn		= [webflow: "1.3.4 => *", jquery: "1.4 => *"]
-	def pluginExcludes	= [
-		"grails-app/views/error.gsp",
-		"web-app/css",
-		"web-app/images",
-		"web-app/js/prototype",
-		"web-app/js/application.js"
-	]
-	def author			= "Jeroen Wesbeek"
-	def authorEmail		= "work@osx.eu"
-	def title			= "This plugin enables Ajaxified Webflows"
-	def description		= '''\\
+    def version		= "0.2.3"
+    def grailsVersion	= "1.3.4 > *"
+    def dependsOn	= [webflow: "1.3.4 => *", jquery: "1.4 => *"]
+    def pluginExcludes	= [
+        	"grails-app/views/error.gsp",
+		"grails-app/conf/DataSource.groovy",
+                "web-app/css",
+                "web-app/images",
+                "web-app/js/prototype",
+                "web-app/js/application.js"
+    ]
+
+    // TODO Fill in these fields
+    def title		= "This plugin enables Ajaxified Webflows"
+    def author		= "Jeroen Wesbeek"
+    def authorEmail	= "work@osx.eu"
+    def description	= '''\\
 This plugin enables Ajaxified Webflows and is able to inject working customizable ajax webflows into your existing project(s)
 '''
-	def documentation	= "https://github.com/4np/grails-ajaxflow/blob/master/README.md"
+    def documentation	= "https://github.com/4np/grails-ajaxflow/blob/master/README.md"
+    def license		= "APACHE"
+    def issueManagement	= [ system: "github", url: "https://github.com/4np/grails-ajaxflow/issues" ]
+    def scm		= [ url: "https://github.com/4np/grails-ajaxflow" ]
 
-	def doWithWebDescriptor = { xml ->
-		// TODO Implement additions to web.xml (optional), this event occurs before
-	}
+    // Details of company behind the plugin (if there is one)
+//    def organization = [ name: "My Company", url: "http://www.my-company.com/" ]
 
-	def doWithSpring = {
-		// TODO Implement runtime spring config (optional)
-	}
+    // Any additional developers beyond the author specified above.
+//    def developers = [ [ name: "Joe Bloggs", email: "joe@bloggs.net" ]]
 
-	def doWithDynamicMethods = { ctx ->
-		// TODO Implement registering dynamic methods to classes (optional)
-	}
+    def doWithWebDescriptor = { xml ->
+        // TODO Implement additions to web.xml (optional), this event occurs before
+    }
 
-	def doWithApplicationContext = { applicationContext ->
-		// TODO Implement post initialization spring config (optional)
-	}
+    def doWithSpring = {
+        // TODO Implement runtime spring config (optional)
+    }
 
-	def onChange = { event ->
-		// TODO Implement code that is executed when any artefact that this plugin is
-		// watching is modified and reloaded. The event contains: event.source,
-		// event.application, event.manager, event.ctx, and event.plugin.
-	}
+    def doWithDynamicMethods = { ctx ->
+        // TODO Implement registering dynamic methods to classes (optional)
+    }
 
-	def onConfigChange = { event ->
-		// TODO Implement code that is executed when the project configuration changes.
-		// The event is the same as for 'onChange'.
-	}
+    def doWithApplicationContext = { applicationContext ->
+        // TODO Implement post initialization spring config (optional)
+    }
+
+    def onChange = { event ->
+        // TODO Implement code that is executed when any artefact that this plugin is
+        // watching is modified and reloaded. The event contains: event.source,
+        // event.application, event.manager, event.ctx, and event.plugin.
+    }
+
+    def onConfigChange = { event ->
+        // TODO Implement code that is executed when the project configuration changes.
+        // The event is the same as for 'onChange'.
+    }
+
+    def onShutdown = { event ->
+        // TODO Implement code that is executed when the application shuts down (optional)
+    }
 }
--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -1,7 +1,9 @@
 grails.project.class.dir = "target/classes"
 grails.project.test.class.dir = "target/test-classes"
 grails.project.test.reports.dir = "target/test-reports"
+grails.project.target.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
+
 grails.project.dependency.resolution = {
     // inherit Grails' default dependencies
     inherits("global") {
@@ -10,14 +12,13 @@ grails.project.dependency.resolution = {
     }
     log "warn" // log level of Ivy resolver, either 'error', 'warn', 'info', 'debug' or 'verbose'
     repositories {
-        grailsPlugins()
-        grailsHome()
         grailsCentral()
+	grailsRepo "http://grails.org/plugins"
 
         // uncomment the below to enable remote dependency resolution
         // from public Maven repositories
-        //mavenLocal()
         //mavenCentral()
+        //mavenLocal()
         //mavenRepo "http://snapshots.repository.codehaus.org"
         //mavenRepo "http://repository.codehaus.org"
         //mavenRepo "http://download.java.net/maven/2/"
@@ -28,8 +29,19 @@ grails.project.dependency.resolution = {
 
         // runtime 'mysql:mysql-connector-java:5.1.5'
     }
+
     plugins {
-	compile ':jquery:latest.integration'
-	compile ':webflow:latest.integration'
+        build(":tomcat:$grailsVersion",
+              ":release:latest.integration") {
+		// plugin only plugin, should not be transitive to the application
+		export = false
+        }
+
+	compile(":jquery:latest.integration",
+		":webflow:latest.integration") {
+		// required for the proper working of this pluging, should be
+		// in the application
+		export = false
+	}
     }
 }
